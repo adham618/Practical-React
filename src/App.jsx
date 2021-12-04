7ff31193aac559267ee3929dd29cd8513892b9d9
adham618 <adhamtarek291@gmail.com>
2021-12-04 12:53:23 +0200

    react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const CustomToast = ({ closeToast }) => {
+  return (
+    <div>
+      something went wrong !
+      <button onClick={closeToast}>close</button>
+    </div>
+  )
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+  const notify = () => toast(<CustomToast />);
   return (
     <div className="App">
+      <button onClick={notify}>Notify me</button>
+      <ToastContainer />
     </div>
   )
 }
