703d2504a5f76c0ad601b4d95371b734da73dbf1
adham618 <adhamtarek291@gmail.com>
2021-12-04 21:59:55 +0200

    color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { useState } from 'react'
+import { SketchPicker } from 'react-color'
 
 function App() {
-
+  const [color, setColor] = useState('#FFF')
+  const [showColorPicker, setShowColorPicker] = useState(false)
   return (
     <div className="App">
+      <button onClick={() => setShowColorPicker(showColorPicker => !showColorPicker)}>{showColorPicker ? "close color picker" : "pick a color"}</button>
+      {showColorPicker && (
+        <SketchPicker color={color} onChange={updatedColor => setColor(updatedColor.hex)} />
+      )}
+      <h2>you picked {color}</h2>
     </div>
   )
 }
