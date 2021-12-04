b044843514f763ebb75a8f7c061a317627b74dcc
adham618 <adhamtarek291@gmail.com>
2021-12-04 14:14:47 +0200

    react toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+// react-toastify
+const CustomToast = ({ closeToast }) => {
+  return (
+    <div>
+      something went wrong !
+      <button onClick={closeToast}>close</button>
+    </div>
+  )
+}
 function App() {
 
+  // react-toastify
+  const notify = () => toast(<CustomToast />);
   return (
     <div className="App">
+      {/* react-toastify */}
+      <button onClick={notify}>Notify me</button>
+      <ToastContainer />
     </div>
   )
 }
