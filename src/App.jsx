8c37d7c45f5d32a24d8245feb7ac13e154b1c3c7
adham618 <adhamtarek291@gmail.com>
2021-12-04 14:40:27 +0200

    tippyjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
 import './App.css'
-import Modal from 'react-modal';
-import { useState } from 'react';
-
-// react-modal
-Modal.setAppElement('#root');
-// custom styles
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    color: 'orange'
-  },
-  overlay: {
-    background: 'grey'
-  }
-};
-
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 function App() {
-  const [modelIsOpen, setIsOpen] = useState(false)
 
   return (
     <div className="App">
-      <button onClick={() => setIsOpen(true)}>open modal</button>
-      <Modal
-        isOpen={modelIsOpen}
-        onRequestClose={() => setIsOpen(false)}
-        shouldCloseOnOverlayClick={false}
-        style={customStyles}
-      >
-        <h2>Modal title</h2>
-        <p>modal body</p>
-        <button onClick={() => setIsOpen(false)}>close</button>
-      </Modal>
+      <Tippy content="hello">
+        <button>Hover</button>
+      </Tippy>
     </div>
   )
 }
