6ea1365dba46e64f22890b2e3acbf2510f931753
adham618 <adhamtarek291@gmail.com>
2021-12-04 14:08:42 +0200

    the base branche

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
 import './App.css'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-// react-tostify
-const CustomToast = ({ closeToast }) => {
-  return (
-    <div>
-      something went wrong !
-      <button onClick={closeToast}>close</button>
-    </div>
-  )
-}
 function App() {
 
-  // react-tostify
-  const notify = () => toast(<CustomToast />);
   return (
     <div className="App">
-      {/* react-tostify */}
-      <button onClick={notify}>Notify me</button>
-      <ToastContainer />
     </div>
   )
 }
