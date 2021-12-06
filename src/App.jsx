79adca3ca00352b8fdc6c9d9118e92c74844e684
adham618 <adhamtarek291@gmail.com>
2021-12-06 18:40:42 +0200

    react-player

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import ReactPlayer from 'react-player'
 function App() {
 
   return (
     <div className="App">
+      <ReactPlayer controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
     </div>
   )
 }
