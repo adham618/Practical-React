e66decc2025a7b8aa7d7cfd46c7fd376cb36173f
adham618 <adhamtarek291@gmail.com>
2021-12-06 16:03:55 +0200

    react-credit-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css'
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
+import { useState } from 'react';
 
 function App() {
-
+  const [number, setNumber] = useState('')
+  const [name, setName] = useState('')
+  const [expiry, setExpiry] = useState('')
+  const [cvc, setCvc] = useState('')
+  const [focus, setFocus] = useState('')
   return (
     <div className="App">
+      <Cards
+        number={number}
+        name={name}
+        expiry={expiry}
+        cvc={cvc}
+        focused={focus}
+      />
+      <form>
+        <input type="tel" name="number" placeholder="Card Number" value={number} onChange={e => setNumber(e.target.value)} onFocus={e => setFocus(e.target.name)} />
+        <input type="text" name="name" placeholder="Name" value={name} onChange={e => setName(e.target.value)} onFocus={e => setFocus(e.target.name)} />
+        <input type="text" name="expiry" placeholder="Expiry Date" value={expiry} onChange={e => setExpiry(e.target.value)} onFocus={e => setFocus(e.target.name)} />
+        <input type="tel" name="cvc" placeholder="CVC Number" value={cvc} onChange={e => setCvc(e.target.value)} onFocus={e => setFocus(e.target.name)} />
+      </form>
     </div>
   )
 }
