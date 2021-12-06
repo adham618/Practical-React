69c2339e8e48711a1bb31ec80e7564b718d1803b
adham618 <adhamtarek291@gmail.com>
2021-12-06 17:35:53 +0200

    react-datepicker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
-
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import { useState } from 'react'
 function App() {
-
+  const [startDate, setStartDate] = useState(null) // new Date()
   return (
     <div className="App">
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="yyyy/M/dd"
+        // minDate={new Date()}
+        // maxDate={new Date()}
+        filterDate={date => date.getDay() !== 6 && date.getDay !== 0}
+        isClearable
+        showYearDropdown
+        scrollableMonthYearDropdown
+      />
     </div>
   )
 }
